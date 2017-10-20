ac876cef9eba8509006391405b7a34271335ccaf
Janet <xuecui918@gmail.com>
2017-10-20 17:13:13 -0400

    weekly sync

---
+++ testing/Testing171019/Conn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testing171019
+{
+    public class Conn
+    {
+        private static readonly Conn instance = new Conn();
+
+        static Conn() { }
+
+        private Conn() { }
+
+        public static Conn Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void PrintNumber(String input = "", int waitTime = 5000)
+        {
+            Console.WriteLine(String.Format("Entering [PrintNumber] for {0}", input));
+            Task.Delay(waitTime);
+            Console.WriteLine(String.Format("Existing [PrintNumber] for {0}", input));
+        }
+    }
+}
---
+++ testing/Testing171019/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testing171019
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Task t1 = new Task(() =>
+            //{
+            //    for (int i = 0; i < 10; i = i + 2)
+            //        //Conn.GetInstance().PrintNumber("FIRST TASK NO." + i.ToString());
+            //});
+            //Task t2 = new Task(() =>
+            //{
+            //    for (int i = 1; i < 10; i = i + 2)
+            //        //Conn.GetInstance().PrintNumber("another task no." + i.ToString(), 1000);
+            //});
+
+            //t1.Start();
+            //t2.Start();
+
+            //t1.Wait();
+            //t2.Wait();
+
+            Console.Write("Done");
+            Console.ReadKey();
+        }
+    }
+}
