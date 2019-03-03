30d7a98f99aa4a6c539857303565b4fbd32adc25
Janet Xue <xchgdzq233@users.noreply.github.com>
2019-03-03 18:55:15 -0500

    weekly sync

---
+++ MyJava/src/com/jx/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package com.jx;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "test", s2 = "haha", s3 = "hmmm";
+	}
+
+}
